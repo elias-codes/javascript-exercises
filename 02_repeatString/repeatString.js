d5022e72ef9895461a65e622e3a3e7657501d40c
Elias Torres <eliasomar.torres@gmail.com>
2022-07-17 10:51:35 -0700

    Adds solution to exercise git push origin main
    q

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text,repeats) {
+    if (repeats>=0) {
+    let textRepeated = "";
+    for (i=0; i<repeats; i++) {
+        textRepeated+=text;
+    }
+    return textRepeated;
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
