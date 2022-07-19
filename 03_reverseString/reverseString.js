b2424b938f3ae1749731e3a1a0f8eb33e79b81c1
Elias Torres <eliasomar.torres@gmail.com>
2022-07-19 07:47:07 -0700

    Adds solution to exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let convertedString = Array.from(originalString);//converts string to array
+    let reversedString = convertedString.reverse();//reverses string
+    reversedString = reversedString.join("");//joins array elements and converts it to string
+    return reversedString;
 };
 
 // Do not edit below this line
