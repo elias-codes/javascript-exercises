dda1a0f76a3fff3f4357bf6d96ff9af890e90a6d
Elias Torres <eliasomar.torres@gmail.com>
2022-07-19 11:22:51 -0700

    Adds solution to exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
 const removeFromArray = function() {
-
-};
+    //let arguments = [[1,2,3], 2, 3];
+    let valuesArr = arguments[0];
+    let removingElements = [];
+    removingElements = Array.from(arguments);
+    removingElements.shift();
+    /*for (i = 1 ; i < arguments.length ; i++) {
+        removingElements[i-1] = arguments[i];
+    }*/
+    let result;
+    result = valuesArr.filter(item => !removingElements.includes(item));
+    console.log(valuesArr);
+    console.log(removingElements);
+    console.log(result);
+    return result;
+    
+    
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
