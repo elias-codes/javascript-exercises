2a7645009fd6d324e67dbc1e7c768a390681ffc0
Elias Torres <eliasomar.torres@gmail.com>
2022-07-20 10:09:00 -0700

    Adds solution to exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(limit1, limit2) {
+    let sumResult = 0;
+    if ( limit1 < 0 || limit2 < 0 || typeof limit1 !== 'number' || typeof limit2 !== 'number' ) {// filters negative and non number values
+        return "ERROR"
+    }
+    if (limit1<=limit2) {
+        for (i = limit1; i <= limit2; i++) {
+            sumResult += i;
+        }
+    }
+    else {
+        for (i = limit2; i <= limit1; i++) {
+            sumResult += i;
+        }
+    }
 
+    return sumResult
 };
 
 // Do not edit below this line
