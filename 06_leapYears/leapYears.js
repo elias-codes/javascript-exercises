f2e62f9ccb0c6978ee7c3f2f1f48bc735e072e82
Elias Torres <eliasomar.torres@gmail.com>
2022-07-20 10:31:23 -0700

    Adds solution to exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let fours = year % 4;
+    let hundreds = year % 100;
+    let fourHundreds = year % 400;
+    let leap;
+    if (fours == 0) {
+        leap = true;
+    }
+    else {
+        leap = false;
+    }
+    if (hundreds == 0) {
+        leap =false;
+    }
+    if (fourHundreds == 0) {
+        leap = true;
+    }
+    return leap;
+    
 };
 
 // Do not edit below this line
